1464200b9fdeb469bc3abe5d0c72bc41f1a0b90f
smhrd <smhrd@211.228.61.214>
2022-03-10 16:40:34 +0900

    jinsu에 변수생성

--- gitTest/src/main/java/gitTest/Test01.java
+++ gitTest/src/main/java/gitTest/Test01.java
@@ -5,6 +5,8 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("첫번재 커밋");
 		// commit : 로컬레포지토리에 실직적으로 변경사항을 저장시키는 역할 
+		System.out.println("두번째 커밋");
+		
 	}
 
 }
