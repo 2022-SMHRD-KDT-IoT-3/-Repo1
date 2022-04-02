9352c31db934a31eff6ed123c220593d6d4a46ce
smhrd <smhrd@121.147.0.185>
2022-04-02 14:40:49 +0900

    아두이노 값받아오기

---
+++ SmartGorani/src/main/java/Model/UssumDAO.java
@@ -0,0 +1,79 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UssumDAO {
+	PreparedStatement psmt = null;
+	Connection conn = null;
+	ResultSet rs = null;
+	int cnt = 0;
+	
+	
+	// DB연결 메소드
+		public void dbconn() {
+			try {
+				System.out.println("테스트");
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+
+				String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+				String dbid = "campus_b_0310_4";
+				String dbpw = "smhrd4";
+				conn = DriverManager.getConnection(url, dbid, dbpw);
+				System.out.println("db연결 성공");
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("db연결 실패");
+			}
+		}
+		
+	// DB연결 해제 메소드
+		public void dbclose() {
+			try {
+				if (rs != null)
+					rs.close();
+				if (psmt != null)
+					psmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+	// DB 데이터 삽입 메소드
+		public void insertUsage(UssumDTO dto) {
+			
+			String sql = "INSERT INTO tbl_battery\n"
+					+ "    (mb_portserial,\n"
+					+ "		battery_capacity, \n"
+					+ "    battery_usage, \n"
+					+ "    battery_date) \n"
+					+ "VALUES\n"
+					+ "    (?,\n"
+					+ "		?, \n"
+					+ "    ?, \n"
+					+ "    sysdate) \n";
+
+			
+			try {
+				psmt = conn.prepareStatement(sql);
+				
+				psmt.setString(1, dto.getMb_portserial());
+				psmt.setInt(2, dto.getBattery_capacity());
+				psmt.setDouble(3, dto.getBattery_usage());
+
+				
+				psmt.executeUpdate();
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+}
---
+++ SmartGorani/src/main/java/arduino/module1.java
@@ -0,0 +1,75 @@
+package arduino;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Model.UssumDAO;
+import Model.UssumDTO;
+import arduino.send;
+
+
+
+@WebServlet("/module1")
+public class module1 extends HttpServlet {
+   protected void service(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
+	  	  
+      response.setContentType("text/html; charset=UTF-8");
+      request.setCharacterEncoding("UTF-8");
+      PrintWriter out = response.getWriter();
+
+      
+
+      String battery = request.getParameter("battery");		//배터리잔량
+      String power1 = request.getParameter("power1");	//배터리사용전력
+      String power2 = request.getParameter("power2");	//일반전기사용전력
+      String timecnt = request.getParameter("timecnt");	 //1초마다 카운트증가
+      String battotalpower = request.getParameter("battotalpower"); //배터리전력총합
+      String electotalpower = request.getParameter("electotalpower"); //일반전기전력총합
+      String port = request.getParameter("port");
+      
+      
+      UssumDTO dto = new UssumDTO(port,Integer.parseInt(battery),Double.parseDouble(battotalpower));
+      UssumDAO dao = new UssumDAO();
+      
+      
+//      if(timecnt.equals("10")) {
+//			dao.dbconn();
+//          dao.insertUsage(dto);
+//      	dao.dbclose();
+//      }
+      
+      
+      
+      String select = send.select;
+      
+      if(select.equals("1")) {
+			out.print("{\"SELECT\":\"1\"}"); //메모장 복붙시 "앞에 자동으로 \넣어줌
+		}else if(select.equals("0")) {
+			out.print("{\"SELECT\":\"0\"}");
+		}
+     
+      
+     
+
+      System.out.println("servlet 배터리 잔량: " + battery + "%");
+      System.out.println("servlet 배터리 사용 전력 : " + power1 + "mW");
+      System.out.println("servlet 일반전기 사용 전력 : " + power2 + "mW");
+      System.out.println("카운트 : "+timecnt);
+      System.out.println("servlet 배터리전력 총합 :"+battotalpower+"mW");
+      System.out.println("servlet 일반전기전력 총합 :"+electotalpower+"mW");
+      System.out.println("port넘버 : "+port);
+	  
+      
+	  
+
+   
+   }
+}
